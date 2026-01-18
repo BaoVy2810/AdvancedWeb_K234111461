21f01ea885121bc0047b424eff096e34c7f49a11
BaoVy2810 <vytnb234112e@st.uel.edu.vn>
2026-01-18 09:32:43 +0700

    add Exercise13, Exercise14, and Exercise19 components with corresponding templates and styles

---
+++ K234112E/my-app/src/app/exercise14/exercise14.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercise14',
+  standalone: false,
+  templateUrl: './exercise14.html',
+  styleUrl: './exercise14.css',
+})
+export class Exercise14 {
+
+}
